bbffa76ebefe7e2d14e75239151436726803a8e9
acrifolium <olivier.azevedo@gmail.com>
2019-11-07 17:48:56 +0100

    2020

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # olippSalonHabitat
 Web site AngularJS / PHP for Salon Habitat
+
+use node version v8.16.2 (nvm install v8)
+
+Build dist folder -> gulp
--- api/Annonceur.php
+++ api/Annonceur.php
@@ -1,54 +1,28 @@
 <?php
 
-	error_reporting(E_ALL);
-	set_time_limit(0);
+$fileHandle = fopen("./data/ANNONCEURS.csv", "r");
+ 
+$annonceurs = array();
 
-	date_default_timezone_set('Europe/London');
+while (($row = fgetcsv($fileHandle, 0, ";")) !== FALSE) {
+	$node = array(); 
+	$node["name"] = $row[0];
+	$node["description"] = $row[1];
 
+	$address = array();
+	$address["contact"] = $row[2];
+	$address["firstline"] = $row[3];
+	$address["postalCode"] = $row[4];
+	$address["city"] = $row[5];
+	$node["address"] = $address;
 
-	/** Include path **/
-	set_include_path(get_include_path() . PATH_SEPARATOR . './PHPExcel/Classes/');
-
-	/** PHPExcel_IOFactory */
-	include 'PHPExcel/IOFactory.php';
-
-	$inputFileName = './data/ANNONCEURS.csv';
-
-	//  Read your Excel workbook
-	try {
-		$objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-	} catch(Exception $e) {
-		$response = array('success' => false, 'message' => 'Fichier des annonceurs invalide');
-    	echo json_encode($response);
-	    exit;
-	}
-
-	$sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-
-	$annonceurs = array();
-
-	for ($row = 1; $row <= $objPHPExcel->getSheet(0)->getHighestRow(); $row++)
-	{
-		$node = array(); 
-		
-		$node["name"] = (string)$sheetData[$row]["A"];
-		$node["description"] = (string)$sheetData[$row]["B"];
-
-		$address = array();
-		$address["contact"] = (string)$sheetData[$row]["C"];
-		$address["firstline"] = (string)$sheetData[$row]["D"];
-		$address["postalCode"] = (string)$sheetData[$row]["E"];
-		$address["city"] = (string)$sheetData[$row]["F"];
-		$node["address"] = $address;
-
-		$node["telephone"] = (string)$sheetData[$row]["G"];
-		$node["portable"] = (string)$sheetData[$row]["H"];
-		$node["mail"] = (string)$sheetData[$row]["I"];
-		$node["webSite"] = (string)$sheetData[$row]["J"];
-		
-		$annonceurs[] = $node;
-	}
-
+	$node["telephone"] = $row[6];
+	$node["portable"] = $row[7];
+	$node["mail"] = $row[8];
+	$node["webSite"] = $row[9];
+	
+	$annonceurs[] = $node;
+}
 	$response = array('success' => true, 'message' => 'Annonceurs loaded', 'annonceurs' => $annonceurs);
 
     echo json_encode($response);
